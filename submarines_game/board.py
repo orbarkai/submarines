6167bf2980787d46e856534785a0e37fd1b8cf42
Or Barkai <orbar2002@gmail.com>
2020-12-30 14:18:06 +0200

    place submarine

--- submarines_game/board.py
+++ submarines_game/board.py
@@ -4,27 +4,37 @@ A game board's functionality
 
 from submarines_game import constatns
 import numpy as np
-import enum
-
-
-@enum.unique
-class Tile(enum.IntEnum):
-    """
-    A tile on the board is represented as an int enum.
-    """
-
-    EMPTY_TILE = 0
-    SUBMARINE_TWO_TILE = 1
-    SUBMARINE_THREE_TILE = 2
-    SUBMARINE_FOUR_TILE = 3
-    SUBMARINE_FIVE_TILE = 4
-
 
 class Board:
     """
     The board keeps memory on all submarines.
     It helps you place submarines and mark tiles
+    Each tile on the board represents the submarine's size
     """
 
     def __init__(self, size: int = constatns.Board.BOARD_SIZE):
-        self.tiles = np.full((size, size), Tile.EMPTY_TILE, dtype=Tile)
+        self._size = size
+        self.tiles = np.full((self._size, self._size), constatns.Board.NO_SUBMARINE, dtype=int)
+
+    def place_submarine(self, column: int, row: int, size: int, horizontal: bool = True):
+        """
+        Place a submarine on the board if possible
+
+        :param row: The submarine's top left position row
+        :param column: The submarine's top left position column
+        :param size: The submarine's size
+        :param horizontal: Is the submarine placed horizontally
+        """
+
+        if horizontal:
+            for x in range(column, column + size):
+                if x < self._size and row < self._size \
+                        and self.tiles[x][row] == constatns.Board.NO_SUBMARINE:
+
+                    self.tiles[x][row] = size
+        else:
+            for y in range(row, row + size):
+                if y < self._size and column < self._size \
+                        and self.tiles[column][y] == constatns.Board.NO_SUBMARINE:
+
+                    self.tiles[column][y] = size
--- submarines_game/constatns.py
+++ submarines_game/constatns.py
@@ -6,3 +6,4 @@ All the game's constatns are placed in this module
 class Board:
 
     BOARD_SIZE = 10
+    NO_SUBMARINE = 0
